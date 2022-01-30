a0f9e4fc270e111ef2e87dc76a1979d70f050be2
clearsky77 <clearsky3264@gmail.com>
2022-01-30 21:33:15 +0900

    UI 헤더 작성

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.btnText}>업무</Text>
+        <Text style={styles.btnText}>여행</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "white",
   },
 });
